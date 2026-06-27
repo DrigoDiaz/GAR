cb8c0295ed48e2a69ea54ae33d36c888ae53116a
Rodrigo Diaz <rdiaz180700@gmail.com>
2026-06-26 19:46:16 -0700

    Currently finished making a rough design of the login page. I will proceed to work on the functionality of the page to make sure that the inputs are being stored & the button is responsive.

--- Global_Anomaly_Registry/src/pages/LoginPage.tsx
+++ Global_Anomaly_Registry/src/pages/LoginPage.tsx
@@ -3,7 +3,26 @@ import './pageStyling/LoginPage.css';
 function LoginPage() {
   return (
     <>
-        <div>Home</div>
+        <header>
+            <h1 id='titleIncrease'>G.A.R</h1>
+            <p id='garIncrease'>Global.Anomaly.Registry</p>
+        </header>
+
+        <main>
+            <div className='adjustDivs'>
+              <p>Employee ID:</p>
+              <input type='text' className='adjustInputs'></input>
+            </div>
+
+            <div className='adjustDivs'>
+              <p>Password:</p>
+              <input type='text' className='adjustInputs'></input>
+            </div>
+
+            <div className='adjustDivs'>
+              <button type='button' id='adjustButton'>Login</button>
+            </div>
+        </main>
     </>
   )
 }
