c2ddf5df2fb4354190b4930fcd5e6c4795028965
Rodrigo Diaz <rdiaz180700@gmail.com>
2026-07-04 07:04:26 -0700

    Been a little busy, but came back to update the homepage's design. At the moment i am planning to try to add some more effects on the homepage to make it feel more old school. I next plan to get the input inform & make the button useful.

--- Global_Anomaly_Registry/src/pages/LoginPage.tsx
+++ Global_Anomaly_Registry/src/pages/LoginPage.tsx
@@ -4,19 +4,19 @@ function LoginPage() {
   return (
     <>
         <header>
-            <h1 id='titleIncrease'>G.A.R</h1>
-            <p id='garIncrease'>Global.Anomaly.Registry</p>
+            <h1 id='titleIncrease'><u>G.A.R</u></h1>
+            <p id='garIncrease'><i>Global.Anomaly.Registry</i></p>
         </header>
 
         <main>
-            <div className='adjustDivs'>
-              <p>Employee ID:</p>
+            <div className='adjustDivs shiftLeft'>
+              <p>G.U.A.R.D ID:</p>
               <input type='text' className='adjustInputs'></input>
             </div>
 
-            <div className='adjustDivs'>
+            <div className='adjustDivs shiftLeft'>
               <p>Password:</p>
-              <input type='text' className='adjustInputs'></input>
+              <input type='password' className='adjustInputs'></input>
             </div>
 
             <div className='adjustDivs'>
