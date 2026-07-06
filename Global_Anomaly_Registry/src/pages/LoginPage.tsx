3cf057aaba814131db8628e18ae51f29896cb0d3
Rodrigo Diaz <rdiaz180700@gmail.com>
2026-07-05 18:11:57 -0700

    Updated the LoginPage to include the old school type of effect for loading in. Additionally I added ways to keep track of the username & password's that are inputted. Finally I made two blank files meant to be used for the next page of the project, the Home Page.

--- Global_Anomaly_Registry/src/pages/LoginPage.tsx
+++ Global_Anomaly_Registry/src/pages/LoginPage.tsx
@@ -1,8 +1,29 @@
 import './pageStyling/LoginPage.css';
+import { useState } from 'react';
 
 function LoginPage() {
+  const approvedUsers: Record<string, string> = {
+    "LF_GUARD892" : "D_Luna$cat12"
+  };
+
+  const [guardID, SetguardID] = useState("");
+  const [password, Setpassword] = useState("");
+
+  function confirmLogin(){
+    if (guardID in approvedUsers){
+      if (password === approvedUsers[guardID]){
+        console.log("User: " + guardID);
+        console.log("Pwd: " + password);
+        console.log("IN THE SYSTEM!");
+      }
+    } else if (!(guardID in approvedUsers)){
+      console.log("NOT IN SYSTEM!");
+    }
+  };
+
   return (
     <>
+      <div className='oldschoolEffect'>
         <header>
             <h1 id='titleIncrease'><u>G.A.R</u></h1>
             <p id='garIncrease'><i>Global.Anomaly.Registry</i></p>
@@ -11,18 +32,25 @@ function LoginPage() {
         <main>
             <div className='adjustDivs shiftLeft'>
               <p>G.U.A.R.D ID:</p>
-              <input type='text' className='adjustInputs'></input>
+              <input type='text' 
+               value={guardID}
+               onChange={(g_id)=> SetguardID(g_id.target.value)}
+              className='adjustInputs'></input>
             </div>
 
             <div className='adjustDivs shiftLeft'>
               <p>Password:</p>
-              <input type='password' className='adjustInputs'></input>
+              <input type='password' 
+               value={password}
+               onChange={(pwd)=> Setpassword(pwd.target.value)}
+              className='adjustInputs'></input>
             </div>
 
             <div className='adjustDivs'>
-              <button type='button' id='adjustButton'>Login</button>
+              <button type='button' id='adjustButton' onClick={confirmLogin}>Login</button>
             </div>
         </main>
+      </div>
     </>
   )
 }
