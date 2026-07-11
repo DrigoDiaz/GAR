ecdfbbe3bff022e0b1f44f2a86f873ac280913f9
Rodrigo Diaz <rdiaz180700@gmail.com>
2026-07-10 19:15:41 -0700

    Added a new folder that will be used for the thread data within the three tabs of the homePage. Additionally, I fixed up the css for the homePage to add the two section that will be present. The threads & the thread Viewer. Finally I added some test data for now to use for testing soon.

--- Global_Anomaly_Registry/src/components/LS_ThreadList.tsx
+++ Global_Anomaly_Registry/src/components/LS_ThreadList.tsx
@@ -1,5 +1,15 @@
+import '../pageStyling/sharedEffects.css';
+import '../pageStyling/LS_ThreadList.css';
+import personalThreads from '../threadData/personalThreads.json';
+
 function LS_ThreadList(){
-    
+    return (
+        <>
+            <div>
+                <p>test a</p>
+            </div>
+        </>
+    )
 };
 
 export default LS_ThreadList;
--- Global_Anomaly_Registry/src/pages/HomePage.tsx
+++ Global_Anomaly_Registry/src/pages/HomePage.tsx
@@ -1,6 +1,7 @@
 import '../pageStyling/HomePage.css';
 import '../pageStyling/sharedEffects.css';
 import TopNavBar from '../components/TopNavBar';
+import LS_ThreadList from '../components/LS_ThreadList';
 
 
 function HomePage() {
@@ -10,6 +11,18 @@ function HomePage() {
         <header>
           <TopNavBar></TopNavBar>
         </header>
+
+        <main id='setMain'>
+          <section id='sectionBorder'>
+            <LS_ThreadList></LS_ThreadList>
+          </section>
+
+          <aside id='asideBorder'>
+            <div id='highlight'>
+              <p>Test B</p>
+            </div>
+          </aside>
+        </main>
       </div>
     </>
   )
