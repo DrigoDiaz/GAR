2b755202b4764ffa50ee2059a906923702759001
Rodrigo Diaz <rdiaz180700@gmail.com>
2026-07-12 20:11:29 -0700

    Finished adding the UNREAD message & styling how the scroll bar appears & works now. Next step is to work on the ThreadViewer side & implement functionality between both the list & the viewer.

--- Global_Anomaly_Registry/src/components/LS_ThreadList.tsx
+++ Global_Anomaly_Registry/src/components/LS_ThreadList.tsx
@@ -3,12 +3,18 @@ import '../pageStyling/LS_ThreadList.css';
 import personalThreads from '../threadData/personalThreads.json';
 
 function LS_ThreadList(){
+    const UNREAD: string = "UNREAD";
+
     return (
         <>
-            <div id='showBorder'>
+            <div id='scrollBar'>
                 {personalThreads.map((thread) => (
                     <div className="generatedDivs" key={thread.id}>
                         <button type="button" className='alignItems'>{thread.title}</button>
+
+                        {thread.status === UNREAD && (
+                            <p className='displayUnread'>*UNREAD*</p>
+                        )}
                     </div>
                 ))}
             </div>
