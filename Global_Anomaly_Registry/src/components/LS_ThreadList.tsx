9c9ab54075c708f115a9fc0fc19cdecd0a16988c
Rodrigo Diaz <rdiaz180700@gmail.com>
2026-07-13 19:29:45 -0700

    I believe I finished getting the thread files to load based on the tab that the viewer has clicked on. It took some time but got it working now with the help of some async functions & useEffect which updates it everytime one of the three main tabs is clicked upon.

--- Global_Anomaly_Registry/src/components/LS_ThreadList.tsx
+++ Global_Anomaly_Registry/src/components/LS_ThreadList.tsx
@@ -1,37 +1,71 @@
 import '../pageStyling/sharedEffects.css';
 import '../pageStyling/LS_ThreadList.css';
-import personalThreads from '../threadData/personalThreads.json';
+import { useEffect, useState } from 'react';
 
+const jsonThreads = import.meta.glob("../threadData/*.json");
+const UNREAD: string = "UNREAD";
+const PERSONAL: string = "PERSONAL";
+const DOCUMENTS: string = "DOCUMENTS";
+const HELP: string = "HELP";
+let currentThread = "";
+
+// Define Props for ThreadList Component Function
 interface ThreadListProps{
     selectedTab: string;
     setSelectedThread: (current_Tab: number) => void;
 }
 
+// Define ThreadList Type with required & optional fields
+interface ThreadList{
+    id: number;
+    title: string;
+    message: string;
+    status: string;
+    classification?: string;
+    protocol?: string;
+}
+
+// Async function to load one of the local json files. Used to show current thread list.
+async function thread_load(json_path: string){
+    const json_loader = jsonThreads[json_path];
+
+    if (!jsonThreads){
+        throw new Error("Thread wasn't found.")
+    }
+
+    const load_object = await json_loader() as {default: ThreadList[]};
+    return load_object.default;
+}
+
+
 function LS_ThreadList({selectedTab, setSelectedThread}: ThreadListProps){
-    const UNREAD: string = "UNREAD";
-    const PERSONAL: string = "PERSONAL";
-    const DOCUMENT: string = "DOCUMENT";
-    const HELP: string = "HELP";
-    let currentThread = "";
+    const [threadList, setThreadList] = useState<ThreadList[]>([]);
 
     if (selectedTab === PERSONAL){
         currentThread = '../threadData/personalThreads.json';
-    } else if (selectedTab === DOCUMENT){
+    } else if (selectedTab === DOCUMENTS){
         currentThread = "../threadData/entriesThreads.json";
     } else if (selectedTab === HELP){
         currentThread = "../threadData/settingThreads.json";
     }
 
-    function displayThread(data_name: string){
+    useEffect(() => {
+        async function updateThreads(){
+            const thread_data = await thread_load(currentThread);
+            setThreadList(thread_data);
+        }
 
-    };
+        updateThreads();
+    }, [currentThread]);
 
     return (
         <>
             <div id='scrollBar'>
-                {personalThreads.map((thread) => (
+                {threadList.map((thread) => (
                     <div className="generatedDivs" key={thread.id}>
-                        <button type="button" className='alignItems'>{thread.title}</button>
+                        <button type="button" className='alignItems' onClick={() => {
+                            setSelectedThread(thread.id);
+                        }}>{thread.title}</button>
 
                         {thread.status === UNREAD && (
                             <p className='displayUnread'>*UNREAD*</p>
